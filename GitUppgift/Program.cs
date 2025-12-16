0e8e19bb01cc726c381bac4694a7ac86128fea08
Bullhoff <daniel.lundell@gmail.com>
2025-12-16 16:01:58 +0100

    did stuff

--- GitUppgift/Program.cs
+++ GitUppgift/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace GitUppgift
+namespace GitUppgift
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World?");
         }
     }
 }
