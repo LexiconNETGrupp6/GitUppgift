f77cb0fe4b24d96c5f0026ce242a156e65aa8938
Mounir Darwich <mounirdarwich22@gmail.com>
2025-12-16 16:07:52 +0100

    Creating Calculater

--- GitUppgift/Program.cs
+++ GitUppgift/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var calc = new Calculation();
+
+            int sum = calc.Add(5, 3);
+            int difference = calc.Subtract(10, 4);
+            int product = calc.Multiply(2, 6);
+            double result = calc.Divide(10, 2);
         }
     }
 }
