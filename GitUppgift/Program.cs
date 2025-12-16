a5e448127866291fccd288b59ab860d80ac12ea9
Jacob <jackan202@gmail.com>
2025-12-16 15:45:04 +0100

    Add project files.

---
+++ GitUppgift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitUppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
