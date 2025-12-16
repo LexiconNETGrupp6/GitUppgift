5a9b8bdcc5ce13f543850017c55eda9ecb742200
Jacob <jackan202@gmail.com>
2025-12-16 16:11:49 +0100

    Changed hello to goodbye

--- GitUppgift/Program.cs
+++ GitUppgift/Program.cs
@@ -4,8 +4,8 @@ namespace GitUppgift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World?");
-            Console.WriteLine("Hello, World?");
+            Console.WriteLine("Goodbye, World?");
+            Console.WriteLine("Goodbye, World?");
         }
     }
 }
