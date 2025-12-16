8e6035a7414367af0650a6d725ac23b9804def94
Jacob <jackan202@gmail.com>
2025-12-16 16:27:11 +0100

    Added overloading methods for doubles

--- GitUppgift/Calculation.cs
+++ GitUppgift/Calculation.cs
@@ -32,6 +32,29 @@ namespace GitUppgift
 
             return (double)a / b;
         }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero");
+
+            return a / b;
+        }
     }
 
 }
